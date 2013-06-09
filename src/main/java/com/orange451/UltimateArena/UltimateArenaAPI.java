7142593ceaeadb6e2cba50e2fc1f81c2c77f102a
Dan Mulloy <dmulloy2@live.com>
2013-06-09 14:43:16 -0400

    Add some explanations to UltimateArenaAPI

--- src/main/java/com/orange451/UltimateArena/UltimateArenaAPI.java
+++ src/main/java/com/orange451/UltimateArena/UltimateArenaAPI.java
@@ -10,52 +10,86 @@ import com.orange451.UltimateArena.Arenas.Objects.ArenaPlayer;
 public class UltimateArenaAPI 
 {
 	public UltimateArena plugin;
-
 	public UltimateArenaAPI(Plugin p)
 	{
 		this.plugin = (UltimateArena)p;
 	}
 
+	/**
+	 * @param p - Player in question
+	 * @return whether or not they're playing in an arena (boolean)
+	 */
 	public boolean isPlayerPlayingArena(Player p)
 	{
 		return plugin.isInArena(p);
 	}
 	
+	/**
+	 * @param p - Player in question
+	 * @return whether or not they are standing in an arena (boolean)
+	 */
 	public boolean isPlayerInArenaLocation(Player p)
 	{
 		return plugin.isInArena(p.getLocation());
 	}
 	
+	/**
+	 * @param p - Player instance
+	 * @return - The player's ArenaPlayer instance
+	 */
 	public ArenaPlayer getArenaPlayer(Player p)
 	{
 		return plugin.getArenaPlayer(p);
 	}
 	
+	/**
+	 * @param loc - Location in question
+	 * @return - Whether or not an arena is at that location
+	 */
 	public boolean isLocationInArena(Location loc)
 	{
 		return plugin.isInArena(loc);
 	}
 
+	/**
+	 * @param a - ArenaPlayer
+	 * @return - The ArenaPlayer's kills
+	 */
 	public int getKills(ArenaPlayer a)
 	{
 		return a.kills;
 	}
 	
+	/**
+	 * @param a - ArenaPlayer
+	 * @return - The ArenaPlayer's deaths
+	 */
 	public int getDeaths(ArenaPlayer a)
 	{
 		return a.deaths;
 	}
 	
+	/**
+	 * @param a - ArenaPlayer
+	 * @return - The ArenaPlayer's team
+	 */
 	public int getTeam(ArenaPlayer a)
 	{
 		return a.team;
 	}
 	
+	/**
+	 * @param a - ArenaPlayer
+	 * @return - The ArenaPlayer's killstreak
+	 */
 	public int getKillStreak(ArenaPlayer a)
 	{
 		return a.killstreak;
 	}
 	
+	/**
+	 * @return UltimateArena plugin
+	 */
 	public UltimateArena getPlugin()
 	{
 		return plugin;
