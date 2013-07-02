f76b5aed5e6fc4a726e343bd51bc68cb22c02a0c
Dan Mulloy <dmulloy2@live.com>
2013-07-02 01:24:43 -0400

    Update for 1.6.1

--- src/main/java/net/dmulloy2/ultimatearena/events/UltimateArenaKillEvent.java
+++ src/main/java/net/dmulloy2/ultimatearena/events/UltimateArenaKillEvent.java
@@ -66,7 +66,7 @@ public class UltimateArenaKillEvent extends Event
 	
 	public int getHeartsLeft()
 	{
-		return killer.player.getHealth() / 2;
+		return (int) (killer.player.getHealth() / 2);
 	}
 
 	@Override
