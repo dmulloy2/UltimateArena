6ba0510593de6c941dc6484a0efe0017700aeeb8
Dan Mulloy <dmulloy2@live.com>
2013-11-12 22:38:59 -0500

    Sort list by total plays

--- src/main/java/net/dmulloy2/ultimatearena/commands/CmdList.java
+++ src/main/java/net/dmulloy2/ultimatearena/commands/CmdList.java
@@ -1,7 +1,11 @@
 package net.dmulloy2.ultimatearena.commands;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
 import net.dmulloy2.ultimatearena.UltimateArena;
 import net.dmulloy2.ultimatearena.arenas.Arena;
@@ -33,8 +37,29 @@ public class CmdList extends UltimateArenaCommand
 		line.append("&3====[ &eUltimateArenas &3]====");
 		lines.add(line.toString());
 
+		// Sort by total plays
+		HashMap<ArenaZone, Integer> playMap = new HashMap<ArenaZone, Integer>();
+
 		for (ArenaZone az : plugin.getLoadedArenas())
 		{
+			playMap.put(az, az.getTimesPlayed());
+		}
+
+		List<Entry<ArenaZone, Integer>> sortedEntries = new ArrayList<Entry<ArenaZone, Integer>>();
+		Collections.sort(sortedEntries, new Comparator<Entry<ArenaZone, Integer>>()
+		{
+			@Override
+			public int compare(final Entry<ArenaZone, Integer> entry1, final Entry<ArenaZone, Integer> entry2)
+			{
+				return -entry1.getValue().compareTo(entry2.getValue());
+			}
+		});
+
+		// Now display them
+		for (Entry<ArenaZone, Integer> entry : sortedEntries)
+		{
+			ArenaZone az = entry.getKey();
+
 			line = new StringBuilder();
 			line.append("&3[&b" + az.getType().getName() + " &eArena&3]");
 			line.append(" &b" + az.getArenaName() + "  ");
