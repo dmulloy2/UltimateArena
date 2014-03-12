42e2d5f06c72496ec4acce28aac51d6da8ecfcc8
Dan Mulloy <dmulloy2@live.com>
2014-03-12 16:11:13 -0400

    [3.0] Add required getters for ArenaType

--- src/main/java/net/dmulloy2/ultimatearena/api/ArenaType.java
+++ src/main/java/net/dmulloy2/ultimatearena/api/ArenaType.java
@@ -4,7 +4,12 @@
 package net.dmulloy2.ultimatearena.api;
 
 import lombok.Getter;
+import lombok.NonNull;
 import net.dmulloy2.ultimatearena.UltimateArena;
+import net.dmulloy2.ultimatearena.arenas.Arena;
+import net.dmulloy2.ultimatearena.creation.ArenaCreator;
+import net.dmulloy2.ultimatearena.types.ArenaConfig;
+import net.dmulloy2.ultimatearena.types.ArenaZone;
 
 /**
  * @author dmulloy2
@@ -13,9 +18,23 @@ import net.dmulloy2.ultimatearena.UltimateArena;
 @Getter
 public abstract class ArenaType
 {
-	private boolean initialized;
+	private boolean enabled;
 	private UltimateArena ultimateArena;
 	private ArenaDescriptionFile description;
+
+	// ---- Required Getters
+
+	@NonNull
+	public abstract ArenaZone getArenaZone();
+
+	@NonNull
+	public abstract ArenaCreator getCreator();
+
+	@NonNull
+	public abstract ArenaConfig getConfig();
+
+	@NonNull
+	public abstract Arena getArena();
 
 	public final String getName()
 	{
