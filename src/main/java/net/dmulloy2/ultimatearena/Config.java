dmulloy2/UltimateArena

commit 939c9d13f59cb993746059f4911bc7c4c73d15d9

Fix bugs with overlapping arenas, optimize flag checks

src/main/java/net/dmulloy2/ultimatearena/Config.java
@@ -73,9 +73,15 @@ public class Config
 	@Key("persistentEntities")
 	@ValueOptions(ValueOption.LIST_UPPER_CASE)
 	public static List<String> persistentEntities = ListUtil.toList(
-			"PLAYER", "PAINTING", "ITEM_FRAME", "VILLAGER"
+			"PAINTING", "ITEM_FRAME", "VILLAGER"
 	);
 
 	@Key("integration.useWorldEdit")
 	public static boolean useWorldEdit = true;
+
+	@Key("debug")
+	public static boolean debug = false;
+
+	@Key("ignoreOverlap")
+	public static boolean ignoreOverlap = false;
 }

src/main/java/net/dmulloy2/ultimatearena/UltimateArena.java
@@ -111,32 +111,33 @@ import org.bukkit.scheduler.BukkitRunnable;
  * @author dmulloy2
  */
 
+@Getter
 public class UltimateArena extends SwornPlugin
 {
-	private @Getter SpectatingHandler spectatingHandler;
-	private @Getter ArenaTypeHandler arenaTypeHandler;
-	private @Getter ResourceHandler resourceHandler;
-	private @Getter MessageHandler messageHandler;
-	private @Getter FileHandler fileHandler;
-	private @Getter SignHandler signHandler;
-	private @Getter GUIHandler guiHandler;
+	private SpectatingHandler spectatingHandler;
+	private ArenaTypeHandler arenaTypeHandler;
+	private ResourceHandler resourceHandler;
+	private MessageHandler messageHandler;
+	private FileHandler fileHandler;
+	private SignHandler signHandler;
+	private GUIHandler guiHandler;
 
 	// Integration
-	private @Getter EssentialsHandler essentialsHandler;
-	private @Getter WorldEditHandler worldEditHandler;
-	private @Getter ProtocolHandler protocolHandler;
-	private @Getter VaultHandler vaultHandler;
+	private EssentialsHandler essentialsHandler;
+	private WorldEditHandler worldEditHandler;
+	private ProtocolHandler protocolHandler;
+	private VaultHandler vaultHandler;
 
-	private @Getter Map<String, ArenaJoinTask> waiting = new HashMap<>();
-	private @Getter List<ArenaCreator> makingArena = new ArrayList<>();
-	private @Getter List<String> pluginsUsingAPI = new ArrayList<>();
-	private @Getter List<ArenaZone> loadedArenas = new ArrayList<>();
-	private @Getter List<ArenaClass> classes = new ArrayList<>();
+	private Map<String, ArenaJoinTask> waiting = new HashMap<>();
+	private List<ArenaCreator> makingArena = new ArrayList<>();
+	private List<String> pluginsUsingAPI = new ArrayList<>();
+	private List<ArenaZone> loadedArenas = new ArrayList<>();
+	private List<ArenaClass> classes = new ArrayList<>();
 
 	private List<String> whitelistedCommands;
 	private List<Arena> activeArenas = new ArrayList<>();
 
-	private MetadataValue uaIdentifier;
+	private MetadataValue identifier;
 	private String prefix;
 
 	private @Getter boolean stopping;
@@ -157,7 +158,7 @@ public class UltimateArena extends SwornPlugin
 		long start = System.currentTimeMillis();
 
 		// Create identifier
-		uaIdentifier = new FixedMetadataValue(this, true);
+		identifier = new FixedMetadataValue(this, true);
 
 		// Register LogHandler
 		logHandler = new LogHandler(this);
@@ -342,11 +343,6 @@ public class UltimateArena extends SwornPlugin
 		return worldEditHandler != null && worldEditHandler.isEnabled();
 	}
 
-	public MetadataValue getUAIdentifier()
-	{
-		return uaIdentifier;
-	}
-
 	@Override
 	public String getPrefix()
 	{
@@ -434,6 +430,9 @@ public class UltimateArena extends SwornPlugin
 	// Load Stuff
 	private void loadArenas()
 	{
+		if (Config.ignoreOverlap)
+			logHandler.log(Level.WARNING, "Ignoring arena overlap. Be warned, this causes problems!");
+
 		File folder = new File(getDataFolder(), "arenas");
 		File[] children = folder.listFiles(new FileFilter()
 		{

src/main/java/net/dmulloy2/ultimatearena/arenas/Arena.java
@@ -98,6 +98,7 @@ public abstract class Arena implements Reloadable
 	protected List<ArenaSpectator> spectators;
 	protected String lastJoin;
 
+	protected List<ArenaPlayer> leaderboard;
 	protected List<String> finalLeaderboard;
 	protected int leaderboardIndex;
 
@@ -250,7 +251,7 @@ public abstract class Arena implements Reloadable
 		// Save vital data
 		ap.savePlayerData();
 
-		player.setMetadata("UA", plugin.getUAIdentifier());
+		player.setMetadata("UA", plugin.getIdentifier());
 
 		ap.clearInventory();
 
@@ -298,6 +299,8 @@ public abstract class Arena implements Reloadable
 		{
 			spawn(ap);
 		}
+
+		this.leaderboard = null;
 	}
 
 	/**
@@ -579,6 +582,11 @@ public abstract class Arena implements Reloadable
 		}
 	}
 
+	public final void clearCache()
+	{
+		this.leaderboard = null;
+	}
+
 	/**
 	 * Called when a player dies.
 	 *
@@ -713,16 +721,19 @@ public abstract class Arena implements Reloadable
 	 * Kills all players within a certain radius of a {@link Location}
 	 *
 	 * @param loc Center {@link Location}
-	 * @param rad Radius to kill within
+	 * @param radius Radius to kill within
 	 */
-	public final void killAllNear(Location loc, double rad)
+	public final void killAllNear(Location loc, double radius)
 	{
-		plugin.debug("Killing all players near {0} in a radius of {1}", Util.locationToString(loc), rad);
+		plugin.debug("Killing all players near {0} in a radius of {1}", Util.locationToString(loc), radius);
+
+		double squared = radius * radius;
 
 		for (ArenaPlayer ap : active)
 		{
-			if (ap.getPlayer().getHealth() > 0.0D && ap.getPlayer().getLocation().distance(loc) < rad)
-				ap.getPlayer().setHealth(0.0D);
+			Player player = ap.getPlayer();
+			if (player.getHealth() > 0.0D && player.getLocation().distanceSquared(loc) < squared)
+				player.setHealth(0.0D);
 		}
 	}
 
@@ -898,6 +909,9 @@ public abstract class Arena implements Reloadable
 			} catch (IndexOutOfBoundsException ex) { }
 		}
 
+		// Invalidate leaderboard cache
+		this.leaderboard = null;
+
 		// Let everyone know why
 		switch (reason)
 		{
@@ -1037,9 +1051,10 @@ public abstract class Arena implements Reloadable
 
 			this.startingAmount = active.size();
 
-			// Setup the leaderboard for leaderboard signs
+			// Setup the leaderboard
 			this.finalLeaderboard = new ArrayList<>(startingAmount);
 			Collections.fill(finalLeaderboard, "");
+			this.leaderboard = null;
 
 			this.leaderboardIndex = startingAmount - 1;
 
@@ -1068,6 +1083,8 @@ public abstract class Arena implements Reloadable
 
 		for (ArenaPlayer ap : getActivePlayers())
 		{
+			Player player = ap.getPlayer();
+
 			// Make sure they're still online
 			if (! ap.isOnline())
 			{
@@ -1076,7 +1093,7 @@ public abstract class Arena implements Reloadable
 			}
 
 			// Kick them if they've reached the death limit and they're alive
-			if (ap.getDeaths() >= maxDeaths && ap.getPlayer().getHealth() > 0.0D)
+			if (ap.getDeaths() >= maxDeaths && player.getHealth() > 0.0D)
 			{
 				ap.leaveArena(LeaveReason.DEATHS);
 				continue;
@@ -1095,9 +1112,9 @@ public abstract class Arena implements Reloadable
 					ap.putData("healTimer", ap.getDataInt("healTimer") - 1);
 					if (ap.getDataInt("healTimer") <= 0)
 					{
-						if (ap.getPlayer().getHealth() > 0 && ap.getPlayer().getHealth() + 1 <= 20)
+						if (player.getHealth() > 0 && player.getHealth() < 20)
 						{
-							ap.getPlayer().setHealth(ap.getPlayer().getHealth() + 1);
+							player.setHealth(Math.max(player.getHealth() + 1, 20));
 							ap.getData().put("healTimer", 2);
 						}
 					}
@@ -1110,8 +1127,8 @@ public abstract class Arena implements Reloadable
 					{
 						for (PotionEffect effect : ac.getPotionEffects())
 						{
-							if (! ap.getPlayer().hasPotionEffect(effect.getType()))
-								ap.getPlayer().addPotionEffect(effect);
+							if (! player.hasPotionEffect(effect.getType()))
+								player.addPotionEffect(effect);
 						}
 					}
 				}
@@ -1221,9 +1238,20 @@ public abstract class Arena implements Reloadable
 			{
 				if (entity != null && entity.isValid())
 				{
-					if (! Config.persistentEntities.contains(entity.getType().name()))
+					if (isInside(entity.getLocation()))
 					{
-						if (isInside(entity.getLocation()))
+						if (entity instanceof Player)
+						{
+							// Players in the arena should already have been teleported
+							// If not, force them to respawn
+							Player player = (Player) entity;
+							player.setHealth(0.0D);
+
+							player.sendMessage(plugin.getPrefix() + FormatUtil.format(getMessage("postgameKick"), name));
+							continue;
+						}
+
+						if (! Config.persistentEntities.contains(entity.getType().name()))
 						{
 							entity.remove();
 							count++;
@@ -1236,7 +1264,7 @@ public abstract class Arena implements Reloadable
 		}
 		catch (Throwable ex)
 		{
-			plugin.getLogHandler().log(Level.WARNING, Util.getUsefulStack(ex, "clearing entities in " + this));
+			plugin.getLogHandler().log(Level.WARNING, Util.getUsefulStack(ex, "clearing entities in " + name));
 		}
 	}
 
@@ -1248,12 +1276,15 @@ public abstract class Arena implements Reloadable
 	}
 
 	/**
-	 * Gets a list of active players, sorted by KDR.
+	 * Gets a list of active players, sorted by KDR. The result is cached and
+	 * {@link #clearCache()} must be called to get a fresh result.
 	 *
-	 * @return The list
+	 * @return The leaderboard
 	 */
 	public List<ArenaPlayer> getLeaderboard()
 	{
+		if (leaderboard != null) return leaderboard;
+		
 		Map<ArenaPlayer, Double> kdrMap = new HashMap<>();
 		for (ArenaPlayer ap : getActivePlayers())
 		{
@@ -1276,7 +1307,7 @@ public abstract class Arena implements Reloadable
 			leaderboard.add(entry.getKey());
 		}
 
-		return leaderboard;
+		return this.leaderboard = leaderboard;
 	}
 
 	/**
@@ -1293,13 +1324,10 @@ public abstract class Arena implements Reloadable
 		int pos = 1;
 		for (ArenaPlayer ap : getLeaderboard())
 		{
-			if (ap != null)
-			{
-				leaderboard.add(FormatUtil.format(getMessage("leaderboard"),
-						pos, decideColor(ap), ap.getName().equals(player.getName()) ? "&l" : "", ap.getName(), ap.getKills(), ap.getDeaths(), ap.getKDR()
-				));
-				pos++;
-			}
+			leaderboard.add(FormatUtil.format(getMessage("leaderboard"),
+					pos, decideColor(ap), ap.getName().equals(player.getName()) ? "&l" : "", ap.getName(), ap.getKills(), ap.getDeaths(), ap.getKDR()
+			));
+			pos++;
 		}
 
 		return leaderboard;

src/main/java/net/dmulloy2/ultimatearena/types/ArenaZone.java
@@ -164,10 +164,27 @@ public class ArenaZone implements Reloadable, ConfigurationSerializable
 		lobby.setParam(lobby1, lobby2);
 		arena.setParam(arena1, arena2);
 
-		// Add to the loaded arenas list
-		plugin.getLoadedArenas().add(this);
+		List<ArenaZone> loaded = plugin.getLoadedArenas();
+
+		if (! Config.ignoreOverlap)
+		{
+			for (ArenaZone that : loaded)
+			{
+				if (checkOverlap(that))
+					plugin.getLogHandler().debug(Level.WARNING, "Arena {0} overlaps with arena {1}!", name, that.name);
+			}
+		}
+
+		loaded.add(this);
+
 		this.loaded = true;
 		return true;
+	}
+
+	private boolean checkOverlap(ArenaZone that)
+	{
+		return that.lobby.checkOverlap(lobby) || that.arena.checkOverlap(arena) ||
+			   that.lobby.checkOverlap(arena) || that.arena.checkOverlap(lobby);
 	}
 
 	// ---- Getters and Setters

src/main/java/net/dmulloy2/ultimatearena/types/Field.java
@@ -59,22 +59,32 @@ public class Field
 
 	public boolean isInside(Location loc)
 	{
-		if (! initialized)
-			return false;
-
 		Validate.notNull(loc, "loc cannot be null!");
+		return isInside(loc.getWorld(), loc.getX(), loc.getY(), loc.getZ());
+	}
 
-		World world = loc.getWorld();
-		if (getWorld().equals(world))
-		{
-			int locX = loc.getBlockX();
-			int locZ = loc.getBlockZ();
+	public boolean isInside(ArenaLocation loc)
+	{
+		Validate.notNull(loc, "loc cannot be null!");
+		return isInside(loc.getWorld(), loc.getX(), loc.getY(), loc.getZ());
+	}
 
-			if (locX >= min.getX() && locX <= max.getX())
-				return locZ >= min.getZ() && locZ <= max.getZ();
-		}
+	protected boolean isInside(World world, double x, double y, double z)
+	{
+		return initialized
+				&& getWorld().equals(world)
+				&& x >= min.getX() && x <= max.getX()
+				&& z >= min.getZ() && z <= max.getZ();	
+	}
 
-		return false;
+	public boolean checkOverlap(Field that)
+	{
+		return isInside(that.min) || isInside(that.max) || that.checkOverlap(this, false);
+	}
+
+	private boolean checkOverlap(Field that, boolean recurse)
+	{
+		return isInside(that.min) || isInside(that.max) || (recurse && that.checkOverlap(this));
 	}
 
 	public Block getBlockAt(int x, int y, int z)

src/main/resources/config.yml
@@ -95,7 +95,6 @@ spectator:
 # Persistent entities
 # These entities won't be removed when the arena stops
 persistentEntities:
-- "PLAYER"
 - "PAINTING"
 - "ITEM_FRAME"
 - "VILLAGER"

src/main/resources/messages.yml
@@ -175,6 +175,7 @@ elimination: "&e{0} &3has been eliminated!"
 youWereKicked: "&cYou have been kicked from the arena!"
 kickBroadcast: "&e{0} &3has been kicked from the arena!"
 errorBroadcast: "&e{0} &3left the arena due to an error!"
+postgameKick: "&3You were removed from the area of &e{0}&3!"
 
 # Scoreboard
 scoreboardHeader: "&3[ &e{0} &3]"

pom.xml
@@ -3,7 +3,7 @@
   <modelVersion>4.0.0</modelVersion>
   <groupId>net.dmulloy2</groupId>
   <artifactId>ultimatearena</artifactId>
-  <version>3.1-SNAPSHOT</version>
+  <version>3.0.1-SNAPSHOT</version>
   <name>UltimateArena</name>
 
   <properties>

commit 2c94319418d14b5ad5d78a96c7dfa14e47b232f2

Fix some issues caused by 939c9d1
This should fix issues with overlapping arenas

Addresses #171

src/main/java/net/dmulloy2/ultimatearena/Config.java
@@ -82,9 +82,9 @@ public class Config
 	@Key("debug")
 	public static boolean debug = false;
 
-	@Key("ignoreOverlap")
-	public static boolean ignoreOverlap = false;
-
 	@Key("fixInvisibility")
 	public static boolean fixInvisibility = false;
+
+	@Key("clearEntities")
+	public static boolean clearEntities = true;
 }

src/main/java/net/dmulloy2/ultimatearena/UltimateArena.java
@@ -430,9 +430,6 @@ public class UltimateArena extends SwornPlugin
 	// Load Stuff
 	private void loadArenas()
 	{
-		if (Config.ignoreOverlap)
-			logHandler.log(Level.WARNING, "Ignoring arena overlap. Be warned, this causes problems!");
-
 		File folder = new File(getDataFolder(), "arenas");
 		File[] children = folder.listFiles(new FileFilter()
 		{

src/main/java/net/dmulloy2/ultimatearena/arenas/Arena.java
@@ -1247,6 +1247,9 @@ public abstract class Arena implements Reloadable
 
 	private final void clearEntities()
 	{
+		if (! Config.clearEntities)
+			return;
+
 		try
 		{
 			int count = 0;
@@ -1254,7 +1257,7 @@ public abstract class Arena implements Reloadable
 			{
 				if (entity != null && entity.isValid())
 				{
-					if (isInside(entity.getLocation()))
+					if (az.isInsideArena(entity.getLocation()))
 					{
 						if (entity instanceof Player)
 						{
@@ -1264,6 +1267,7 @@ public abstract class Arena implements Reloadable
 							player.setHealth(0.0D);
 
 							player.sendMessage(plugin.getPrefix() + FormatUtil.format(getMessage("postgameKick"), name));
+							plugin.getLogHandler().log("{0} was removed from the {1} arena", player.getName(), name);
 							continue;
 						}
 
@@ -1280,7 +1284,7 @@ public abstract class Arena implements Reloadable
 		}
 		catch (Throwable ex)
 		{
-			plugin.getLogHandler().log(Level.WARNING, Util.getUsefulStack(ex, "clearing entities in " + name));
+			plugin.getLogHandler().log(Level.WARNING, Util.getUsefulStack(ex, "clearing entities from " + name));
 		}
 	}
 

src/main/java/net/dmulloy2/ultimatearena/types/ArenaZone.java
@@ -165,26 +165,10 @@ public class ArenaZone implements Reloadable, ConfigurationSerializable
 		arena.setParam(arena1, arena2);
 
 		List<ArenaZone> loaded = plugin.getLoadedArenas();
-
-		if (! Config.ignoreOverlap)
-		{
-			for (ArenaZone that : loaded)
-			{
-				if (checkOverlap(that))
-					plugin.getLogHandler().debug(Level.WARNING, "Arena {0} overlaps with arena {1}!", name, that.name);
-			}
-		}
-
 		loaded.add(this);
 
 		this.loaded = true;
 		return true;
-	}
-
-	private boolean checkOverlap(ArenaZone that)
-	{
-		return that.lobby.checkOverlap(lobby) || that.arena.checkOverlap(arena) ||
-			   that.lobby.checkOverlap(arena) || that.arena.checkOverlap(lobby);
 	}
 
 	// ---- Getters and Setters
@@ -268,39 +252,17 @@ public class ArenaZone implements Reloadable, ConfigurationSerializable
 
 	// ---- Utility Methods
 
-	/**
-	 * Checks if a location is inside this arena.
-	 *
-	 * @param location {@link Location} to check
-	 * @return True if the location is inside the arena, false if not
-	 */
-	public boolean isInside(Location location)
+	public final boolean isInside(Location loc)
 	{
-		Validate.notNull(location, "location cannot be null!");
-
-		World world = getWorld();
-
-		try
-		{
-			return world.equals(location.getWorld()) && (lobby.isInside(location) || arena.isInside(location));
-		}
-		catch (Throwable ex)
-		{
-			if (! insideStackPrinted)
-			{
-				plugin.getLogHandler().log(Level.WARNING, Util.getUsefulStack(ex, "performing location check for " + name));
-
-				if (world == null)
-					plugin.getLogHandler().log(Level.WARNING, "This is caused by a null world. Does world {0} exist?", worldName);
-
-				insideStackPrinted = true;
-			}
-
-			return false;
-		}
+		Validate.notNull(loc, "loc cannot be null!");
+		return lobby.isInside(loc) || arena.isInside(loc);
 	}
 
-	private transient boolean insideStackPrinted;
+	public final boolean isInsideArena(Location loc)
+	{
+		Validate.notNull(loc, "loc cannot be null!");
+		return arena.isInside(loc);
+	}
 
 	/**
 	 * Gets whether or not a player has voted

src/main/java/net/dmulloy2/ultimatearena/types/Field.java
@@ -20,13 +20,13 @@ package net.dmulloy2.ultimatearena.types;
 
 import java.util.List;
 
-import lombok.Getter;
-
 import org.apache.commons.lang.Validate;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+
+import lombok.Getter;
 
 /**
  * @author dmulloy2
@@ -35,46 +35,48 @@ import org.bukkit.block.Block;
 @Getter
 public class Field
 {
-	protected boolean initialized;
-
-	protected ArenaLocation max;
+	protected boolean init;
 	protected ArenaLocation min;
+	protected ArenaLocation max;
+
+	public Field(ArenaLocation loc1, ArenaLocation loc2)
+	{
+		setParam(loc1, loc2);
+	}
 
 	public Field() { }
 
-	public Field(ArenaLocation point1, ArenaLocation point2)
+	public void setParam(ArenaLocation loc1, ArenaLocation loc2)
 	{
-		setParam(point1, point2);
+		Validate.notNull(loc1, "loc1 cannot be null!");
+		Validate.notNull(loc2, "loc2 cannot be null!");
+
+		this.min = ArenaLocation.getMinimum(loc1, loc2);
+		this.max = ArenaLocation.getMaximum(loc1, loc2);
+		this.init = true;
 	}
 
-	public void setParam(ArenaLocation point1, ArenaLocation point2)
+	public World getWorld()
 	{
-		Validate.notNull(point1, "point1 cannot be null!");
-		Validate.notNull(point2, "point2 cannot be null!");
-
-		this.max = ArenaLocation.getMaximum(point1, point2);
-		this.min = ArenaLocation.getMinimum(point1, point2);
-		this.initialized = true;
+		return max.getWorld();
 	}
 
 	public boolean isInside(Location loc)
 	{
 		Validate.notNull(loc, "loc cannot be null!");
-		return isInside(loc.getWorld(), loc.getX(), loc.getY(), loc.getZ());
+
+		int x = loc.getBlockX();
+		int z = loc.getBlockZ();
+
+		return init
+				&& loc.getWorld().equals(getWorld())
+				&& x <= max.getX() && x >= min.getX()
+				&& z <= max.getZ() && z >= min.getZ();
 	}
 
-	public boolean isInside(ArenaLocation loc)
+	private boolean isInside(ArenaLocation loc)
 	{
-		Validate.notNull(loc, "loc cannot be null!");
-		return isInside(loc.getWorld(), loc.getX(), loc.getY(), loc.getZ());
-	}
-
-	protected boolean isInside(World world, double x, double y, double z)
-	{
-		return initialized
-				&& getWorld().equals(world)
-				&& x >= min.getX() && x <= max.getX()
-				&& z >= min.getZ() && z <= max.getZ();	
+		return isInside(loc.getLocation());
 	}
 
 	public boolean checkOverlap(Field that)
@@ -87,55 +89,28 @@ public class Field
 		return isInside(that.min) || isInside(that.max) || (recurse && that.checkOverlap(this));
 	}
 
-	public Block getBlockAt(int x, int y, int z)
+	public void removeMaterials(List<Material> clear)
 	{
-		return getWorld().getBlockAt(min.getX() + x, min.getY() + y, min.getZ() + z);
-	}
-
-	public final void setType(Material mat)
-	{
-		World world = getWorld();
-		for (int x = min.getX(); x < max.getX() + 1; x++)
+		for (int x = min.getX(); x <= max.getX(); x++)
 		{
-			for (int y = min.getY(); y < max.getY() + 1; y++)
+			for (int y = min.getY(); y <= max.getY(); y++)
 			{
-				for (int z = min.getZ(); z < max.getZ() + 1; z++)
+				for (int z = min.getZ(); z <= max.getZ(); z++)
 				{
-					Block block = world.getBlockAt(x, y, z);
-					block.setType(mat);
-				}
-			}
-		}
-	}
-
-	public void removeMaterials(List<Material> materials)
-	{
-		World world = getWorld();
-		for (int x = min.getX(); x < max.getX() + 1; x++)
-		{
-			for (int y = min.getY(); y < max.getY() + 1; y++)
-			{
-				for (int z = min.getZ(); z < max.getZ() + 1; z++)
-				{
-					Block block = world.getBlockAt(x, y, z);
-					if (materials.contains(block.getType()))
+					Block block = getWorld().getBlockAt(x, y, z);
+					if (clear.contains(block.getType()))
 						block.setType(Material.AIR);
 				}
 			}
 		}
 	}
 
-	public final World getWorld()
-	{
-		return max.getWorld();
-	}
-
-	public final int getWidth()
+	public int getWidth()
 	{
 		return max.getX() - min.getX();
 	}
 
-	public final int getLength()
+	public int getLength()
 	{
 		return max.getZ() - min.getZ();
 	}

src/main/java/net/dmulloy2/ultimatearena/types/Field3D.java
@@ -18,9 +18,12 @@
  */
 package net.dmulloy2.ultimatearena.types;
 
-import lombok.Getter;
-
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+
+import lombok.Getter;
 
 /**
  * @author dmulloy2
@@ -48,13 +51,34 @@ public final class Field3D extends Field
 		return false;
 	}
 
-	public final boolean isUnder(Location loc)
+	public boolean isUnder(Location loc)
 	{
 		return super.isInside(loc) && loc.getBlockY() < min.getY();
 	}
 
-	public final int getHeight()
+	public int getHeight()
 	{
 		return max.getY() - min.getY();
+	}
+
+	public Block getBlockAt(int x, int y, int z)
+	{
+		return getWorld().getBlockAt(min.getX() + x, min.getY() + y, min.getZ() + z);
+	}
+
+	public final void setType(Material mat)
+	{
+		World world = getWorld();
+		for (int x = min.getX(); x <= max.getX(); x++)
+		{
+			for (int y = min.getY(); y <= max.getY(); y++)
+			{
+				for (int z = min.getZ(); z <= max.getZ(); z++)
+				{
+					Block block = world.getBlockAt(x, y, z);
+					block.setType(mat);
+				}
+			}
+		}
 	}
 }

src/main/resources/config.yml
@@ -106,3 +106,7 @@ integration:
 
 # Sometimes happens with naughty plugins
 fixInvisibility: false
+
+# Whether or not to clear entities after an arena stops
+# If you have issues with overlapping arenas disable this
+clearEntities: true

src/main/resources/messages.yml
@@ -175,7 +175,7 @@ elimination: "&e{0} &3has been eliminated!"
 youWereKicked: "&cYou have been kicked from the arena!"
 kickBroadcast: "&e{0} &3has been kicked from the arena!"
 errorBroadcast: "&e{0} &3left the arena due to an error!"
-postgameKick: "&3You were removed from the area of &e{0}&3!"
+postgameKick: "&3You were removed from the &e{0} &3arena!"
 
 # Scoreboard
 scoreboardHeader: "&3[ &e{0} &3]"